9eb58f5db4a0caa9cea6b538a96cda6e7d8fd04a
josephjquinn <jquinn2005@outlook.com>
2025-04-22 17:03:34 -0500

    removing oauth for production env

--- client/src/lib/foundry.ts
+++ client/src/lib/foundry.ts
@@ -1,11 +1,7 @@
 import { Client, createClient } from "@osdk/client";
-import { createPublicOauthClient } from "@osdk/oauth";
 
 const url = import.meta.env.VITE_FOUNDRY_API_URL;
-const clientId = import.meta.env.VITE_FOUNDRY_CLIENT_ID;
-const redirectUrl = import.meta.env.DEV
-  ? import.meta.env.VITE_FOUNDRY_REDIRECT_URL
-  : import.meta.env.VITE_FOUNDRY_REDIRECT_URL_PROD;
+const token = import.meta.env.VITE_FOUNDRY_TOKEN;
 
 function checkEnv(
   value: string | undefined,
@@ -17,38 +13,15 @@ function checkEnv(
 }
 
 checkEnv(url, "VITE_FOUNDRY_API_URL");
-checkEnv(clientId, "VITE_FOUNDRY_CLIENT_ID");
-checkEnv(
-  redirectUrl,
-  import.meta.env.DEV
-    ? "VITE_FOUNDRY_REDIRECT_URL"
-    : "VITE_FOUNDRY_REDIRECT_URL_PROD"
-);
+checkEnv(token, "VITE_FOUNDRY_TOKEN");
 
 const ontologyRid =
   "ri.ontology.main.ontology.ab3b7be9-b65d-4d93-b68c-c5fc218d81e0";
 
-const scopes: string[] = [
-  "api:ontologies-read",
-  "api:ontologies-write",
-  "api:mediasets-read",
-  "api:mediasets-write",
-];
-
-export const auth = createPublicOauthClient(
-  clientId,
-  url,
-  redirectUrl,
-  true,
-  undefined,
-  window.location.toString(),
-  scopes
-);
-
 const client: Client = createClient(
   import.meta.env.DEV ? "http://localhost:5173" : url,
   ontologyRid,
-  auth
+  () => Promise.resolve(token)
 );
 
 export default client;
