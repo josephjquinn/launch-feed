e1a82e1ce2c3ec93084b5d98665d6f5f4903177e
josephjquinn <jquinn2005@outlook.com>
2025-04-22 14:21:08 -0500

    removing sematic search page from routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import DailyReport from "./pages/DailyReport";
 import NewsSearch from "./pages/NewsSearch";
-import SemanticSearch from "./pages/SemanticSearch";
 import DateGuessGame from "./pages/DateGuessGame";
 import TopHeadlines from "./pages/TopHeadlines";
 
@@ -16,7 +15,6 @@ function App() {
           <Route path="daily-report" element={<DailyReport />} />
           <Route path="news-search" element={<NewsSearch />} />
           <Route path="top-headlines" element={<TopHeadlines />} />
-          <Route path="semantic-search" element={<SemanticSearch />} />
           <Route path="date-guess-game" element={<DateGuessGame />} />
         </Route>
       </Routes>
